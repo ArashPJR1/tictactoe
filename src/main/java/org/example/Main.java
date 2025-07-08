54afc1b44645774ae58203393bc885a4b83ce108
ArashPJR1 <wi24b168@technikum-wien.at>
2025-07-08 14:18:49 +0200

    Projekt fertiggestellt

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TicTacToe game = new TicTacToe();
+
+        System.out.println("Willkommen zu Tic-Tac-Toe!");
+
+        while (true) {
+            game.printBoard();
+            System.out.println("Spieler " + game.getCurrentPlayer() + ", gib deine Zeile (0-2): ");
+            int row = scanner.nextInt();
+            System.out.println("Spieler " + game.getCurrentPlayer() + ", gib deine Spalte (0-2): ");
+            int col = scanner.nextInt();
+
+            if (!game.makeMove(row, col)) {
+                System.out.println("Ungültiger Zug, versuch es erneut!");
+                continue;
+            }
+
+            if (game.checkWin()) {
+                game.printBoard();
+                System.out.println("Spieler " + game.getCurrentPlayer() + " hat gewonnen!");
+                break;
+            }
+
+            if (game.isBoardFull()) {
+                game.printBoard();
+                System.out.println("Unentschieden!");
+                break;
+            }
+
+            game.switchPlayer();
+        }
+
+        scanner.close();
+    }
+}
